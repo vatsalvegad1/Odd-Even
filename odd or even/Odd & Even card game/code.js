009c6c96f649417ee2ab10cd688c83f7de0a5efb
vatsalvegad1 <79971070+vatsalvegad1@users.noreply.github.com>
2021-03-23 14:55:21 +0530

    Add files via upload

---
+++ odd or even/Odd & Even card game/code.js
@@ -0,0 +1,836 @@
+onEvent("start", "click", function( ) {
+  setScreen("screen2");
+  playSound("assets/category_movement/magic_cartoon_game_key_pick_up_1.mp3", false);
+  
+});
+
+setImageURL("image1", "https://playingcardcollector.files.wordpress.com/2013/05/origins_playing_cards_back.jpg");
+setImageURL("image2", "https://playingcardcollector.files.wordpress.com/2013/05/origins_playing_cards_back.jpg");
+setImageURL("image3", "https://playingcardcollector.files.wordpress.com/2013/05/origins_playing_cards_back.jpg");
+setImageURL("image4", "https://playingcardcollector.files.wordpress.com/2013/05/origins_playing_cards_back.jpg");
+setImageURL("image5", "https://playingcardcollector.files.wordpress.com/2013/05/origins_playing_cards_back.jpg");
+setImageURL("image6", "https://playingcardcollector.files.wordpress.com/2013/05/origins_playing_cards_back.jpg");
+setImageURL("image7", "https://playingcardcollector.files.wordpress.com/2013/05/origins_playing_cards_back.jpg");
+setImageURL("image8", "https://playingcardcollector.files.wordpress.com/2013/05/origins_playing_cards_back.jpg");
+setImageURL("image9", "https://playingcardcollector.files.wordpress.com/2013/05/origins_playing_cards_back.jpg");
+setImageURL("image10", "https://playingcardcollector.files.wordpress.com/2013/05/origins_playing_cards_back.jpg");
+setImageURL("startimg", "assets/61hFVdt7F0L.png");
+
+onEvent("image1", "click", function( ) {
+  setImageURL("image1","assets/0f6265fe8dc448b8f032f161db77c033.png");
+});
+onEvent("image2", "click", function( ) {
+  setImageURL("image2","assets/2e36ca132a4d2c082bb882f6ab7f1eac.png");
+});
+
+onEvent("image3", "click", function( ) {
+  setImageURL("image3","assets/download.png");
+});
+
+onEvent("image4", "click", function( ) {
+  setImageURL("image4","download-(1).png");
+});
+
+onEvent("image5", "click", function( ) {
+  setImageURL("image5","download-(4).png");
+});
+
+onEvent("image6", "click", function( ) {
+  setImageURL("image6","assets/number_10bw_p.png");
+});
+
+onEvent("image7", "click", function( ) {
+  setImageURL("image7","download-(5).png");
+});
+
+onEvent("image8", "click", function( ) {
+  setImageURL("image8","download-(3).png");
+});
+
+onEvent("image9", "click", function( ) {
+  setImageURL("image9","download-(6).png");
+});
+
+onEvent("image10", "click", function( ) {
+  setImageURL("image10","download-(2).png");
+});
+
+onEvent("image1", "click", function( ) {
+  onEvent("image2","click", function(){
+  var x=1;
+  var y=6;
+   x + y;
+   setScreen("win");
+  });
+});
+ onEvent("image1", "click", function( ) {
+  onEvent("image3","click", function(){
+  var x=1;
+  var y=2;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image1", "click", function( ) {
+  onEvent("image6","click", function(){
+  var x=1;
+  var y=10;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image1", "click", function( ) {
+  onEvent("image7","click", function(){
+  var x=1;
+  var y=8;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image1", "click", function( ) {
+  onEvent("image10","click", function(){
+  var x=1;
+  var y=4;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image4", "click", function( ) {
+  onEvent("image2","click", function(){
+  var x=3;
+  var y=6;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image4", "click", function( ) {
+  onEvent("image3","click", function(){
+  var x=3;
+  var y=2;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image4", "click", function( ) {
+  onEvent("image6","click", function(){
+  var x=3;
+  var y=10;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image4", "click", function( ) {
+  onEvent("image7","click", function(){
+  var x=3;
+  var y=8;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image4", "click", function( ) {
+  onEvent("image10","click", function(){
+  var x=3;
+  var y=4;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image5", "click", function( ) {
+  onEvent("image2","click", function(){
+  var x=7;
+  var y=6;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image5", "click", function( ) {
+  onEvent("image3","click", function(){
+  var x=7;
+  var y=2;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image5", "click", function( ) {
+  onEvent("image6","click", function(){
+  var x=7;
+  var y=10;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image4", "click", function( ) {
+  onEvent("image7","click", function(){
+  var x=7;
+  var y=8;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image4", "click", function( ) {
+  onEvent("image10","click", function(){
+  var x=7;
+  var y=4;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image6", "click", function( ) {
+  onEvent("image1","click", function(){
+  var x=10;
+  var y=1;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image6", "click", function( ) {
+  onEvent("image4","click", function(){
+  var x=3;
+  var y=10;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image6", "click", function( ) {
+  onEvent("image5","click", function(){
+  var x=7;
+  var y=10;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image6", "click", function( ) {
+  onEvent("image8","click", function(){
+  var x=5;
+  var y=10;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image6", "click", function( ) {
+  onEvent("image9","click", function(){
+  var x=9;
+  var y=10;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image7", "click", function( ) {
+  onEvent("image1","click", function(){
+  var x=8;
+  var y=1;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image7", "click", function( ) {
+  onEvent("image4","click", function(){
+  var x=3;
+  var y=8;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image7", "click", function( ) {
+  onEvent("image6","click", function(){
+  var x=8;
+  var y=7;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image7", "click", function( ) {
+  onEvent("image8","click", function(){
+  var x=8;
+  var y=5;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image7", "click", function( ) {
+  onEvent("image9","click", function(){
+  var x=8;
+  var y=9;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image8", "click", function( ) {
+  onEvent("image2","click", function(){
+  var x=5;
+  var y=6;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image8", "click", function( ) {
+  onEvent("image3","click", function(){
+  var x=5;
+  var y=2;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image8", "click", function( ) {
+  onEvent("image6","click", function(){
+  var x=5;
+  var y=10;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image8", "click", function( ) {
+  onEvent("image7","click", function(){
+  var x=5;
+  var y=8;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image8", "click", function( ) {
+  onEvent("image10","click", function(){
+  var x=5;
+  var y=4;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image9", "click", function( ) {
+  onEvent("image2","click", function(){
+  var x=9;
+  var y=6;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image9", "click", function( ) {
+  onEvent("image3","click", function(){
+  var x=9;
+  var y=2;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image9", "click", function( ) {
+  onEvent("image6","click", function(){
+  var x=9;
+  var y=10;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image9", "click", function( ) {
+  onEvent("image7","click", function(){
+  var x=9;
+  var y=8;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image9", "click", function( ) {
+  onEvent("image10","click", function(){
+  var x=9;
+  var y=4;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image10", "click", function( ) {
+  onEvent("image1","click", function(){
+  var x=4;
+  var y=1;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image10", "click", function( ) {
+  onEvent("image4","click", function(){
+  var x=4;
+  var y=3;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image10", "click", function( ) {
+  onEvent("image4","click", function(){
+  var x=4;
+  var y=7;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image10", "click", function( ) {
+  onEvent("image7","click", function(){
+  var x=4;
+  var y=5;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image10", "click", function( ) {
+  onEvent("image9","click", function(){
+  var x=4;
+  var y=9;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image2", "click", function( ) {
+  onEvent("image1","click", function(){
+  var x=6;
+  var y=1;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image2", "click", function( ) {
+  onEvent("image4","click", function(){
+  var x=6;
+  var y=3;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image2", "click", function( ) {
+  onEvent("image5","click", function(){
+  var x=6;
+  var y=7;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image2", "click", function( ) {
+  onEvent("image8","click", function(){
+  var x=6;
+  var y=5;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image2", "click", function( ) {
+  onEvent("image9","click", function(){
+  var x=6;
+  var y=9;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image3", "click", function( ) {
+  onEvent("image1","click", function(){
+  var x=2;
+  var y=1;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image3", "click", function( ) {
+  onEvent("image4","click", function(){
+  var x=2;
+  var y=3;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image3", "click", function( ) {
+  onEvent("image5","click", function(){
+  var x=2;
+  var y=7;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image3", "click", function( ) {
+  onEvent("image8","click", function(){
+  var x=2;
+  var y=5;
+   x + y;
+   setScreen("win");
+  });
+});
+onEvent("image3", "click", function( ) {
+  onEvent("image9","click", function(){
+  var x=2;
+  var y=9;
+   x + y;
+   setScreen("win");
+  });
+});
+
+//lose
+//lose
+//lose
+
+onEvent("image1", "click", function( ) {
+  onEvent("image4","click", function(){
+  var x=1;
+  var y=3;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image1", "click", function( ) {
+  onEvent("image5","click", function(){
+  var x=1;
+  var y=7;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image1", "click", function( ) {
+  onEvent("image8","click", function(){
+  var x=1;
+  var y=5;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image1", "click", function( ) {
+  onEvent("image9","click", function(){
+  var x=1;
+  var y=9;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image2", "click", function( ) {
+  onEvent("image3","click", function(){
+  var x=6;
+  var y=2;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image2", "click", function( ) {
+  onEvent("image6","click", function(){
+  var x=6;
+  var y=10;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image2", "click", function( ) {
+  onEvent("image7","click", function(){
+  var x=6;
+  var y=8;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image2", "click", function( ) {
+  onEvent("image10","click", function(){
+  var x=6;
+  var y=4;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image3", "click", function( ) {
+  onEvent("image2","click", function(){
+  var x=2;
+  var y=6;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image3", "click", function( ) {
+  onEvent("image6","click", function(){
+  var x=2;
+  var y=10;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image3", "click", function( ) {
+  onEvent("image7","click", function(){
+  var x=2;
+  var y=8;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image3", "click", function( ) {
+  onEvent("image10","click", function(){
+  var x=2;
+  var y=4;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image4", "click", function( ) {
+  onEvent("image1","click", function(){
+  var x=3;
+  var y=1;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image4", "click", function( ) {
+  onEvent("image5","click", function(){
+  var x=3;
+  var y=7;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image4", "click", function( ) {
+  onEvent("image8","click", function(){
+  var x=3;
+  var y=5;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image4", "click", function( ) {
+  onEvent("image9","click", function(){
+  var x=3;
+  var y=9;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image5", "click", function( ) {
+  onEvent("image1","click", function(){
+  var x=7;
+  var y=1;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image5", "click", function( ) {
+  onEvent("image4","click", function(){
+  var x=7;
+  var y=3;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image5", "click", function( ) {
+  onEvent("image8","click", function(){
+  var x=7;
+  var y=5;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image5", "click", function( ) {
+  onEvent("image9","click", function(){
+  var x=7;
+  var y=9;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image6", "click", function( ) {
+  onEvent("image2","click", function(){
+  var x=10;
+  var y=6;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image6", "click", function( ) {
+  onEvent("image3","click", function(){
+  var x=10;
+  var y=3;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image6", "click", function( ) {
+  onEvent("image7","click", function(){
+  var x=10;
+  var y=8;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image6", "click", function( ) {
+  onEvent("image10","click", function(){
+  var x=10;
+  var y=4;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image7", "click", function( ) {
+  onEvent("image2","click", function(){
+  var x=8;
+  var y=6;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image7", "click", function( ) {
+  onEvent("image3","click", function(){
+  var x=8;
+  var y=2;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image7", "click", function( ) {
+  onEvent("image6","click", function(){
+  var x=8;
+  var y=10;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image7", "click", function( ) {
+  onEvent("image10","click", function(){
+  var x=8;
+  var y=4;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image8", "click", function( ) {
+  onEvent("image1","click", function(){
+  var x=5;
+  var y=1;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image8", "click", function( ) {
+  onEvent("image4","click", function(){
+  var x=5;
+  var y=3;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image8", "click", function( ) {
+  onEvent("image5","click", function(){
+  var x=5;
+  var y=7;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image8", "click", function( ) {
+  onEvent("image9","click", function(){
+  var x=5;
+  var y=9;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image9", "click", function( ) {
+  onEvent("image1","click", function(){
+  var x=9;
+  var y=1;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image9", "click", function( ) {
+  onEvent("image4","click", function(){
+  var x=9;
+  var y=3;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image9", "click", function( ) {
+  onEvent("image5","click", function(){
+  var x=9;
+  var y=7;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image9", "click", function( ) {
+  onEvent("image8","click", function(){
+  var x=9;
+  var y=5;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image10", "click", function( ) {
+  onEvent("image2","click", function(){
+  var x=4;
+  var y=6;
+   x + y;
+   setScreen("lose");
+    
+  });
+});
+onEvent("image10", "click", function( ) {
+  onEvent("image3","click", function(){
+  var x=4;
+  var y=2;
+   x + y;
+   setScreen("lose");
+   
+  });
+});
+onEvent("image10", "click", function( ) {
+  onEvent("image6","click", function(){
+  var x=4;
+  var y=10;
+   x + y;
+   setScreen("lose");
+   
+  });
+
+});
+onEvent("image10", "click", function( ) {
+  onEvent("image7","click", function(){
+  var x=4;
+  var y=10;
+   x + y;
+   setScreen("lose");
+   
+
+  });
+});
+playSound("assets/category_slide/whoosh_4.mp3", false);
+
+onEvent("win", "mouseover", function( ) {
+  playSound("assets/applause4.mp3", false);
+  
+});
+onEvent("lose", "mouseover", function( ) {
+  playSound("assets/zapsplat_multimedia_game_sound_fun_arcade_organ_short_negative_fail_lose_001_54274.mp3", false);
+  
+});
+
+
